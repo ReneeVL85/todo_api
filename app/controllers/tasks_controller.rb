d0d690641483f84f913740d89ca08a9ea5d69ec2
ReneeVL85 <renee.v.leijen@gmail.com>
2016-04-25 16:26:00 +0200

    Task Controller and Routing

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,45 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.order(created_at: :asc)
+    render json: {tasks: @tasks}
+  end
+
+  def create
+    @tasks = Task.new(task_params)
+
+    if @task.save
+      render json: {tasks: @task, location: task_url(@task)}, status: :created
+    else
+      render json: {errors: @task.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    render json: {task: @task}
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+       render json: {task: @task}, status: :accepted
+    else
+      render json: {errors: @task.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task = Taks.find(params[:id])
+    if @task.destroy
+      render json: {task: nil}, status: :accepted
+    else
+      render json: {errors: @task.errors}, status: :unprocessable_entity
+    end
+  end
+
+  protected
+
+  def task_params
+    params.require(:task).permit(:title, :completed)
+  end
+end
